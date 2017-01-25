6245a2e1cf7a84b0e9215fa99cebff83e797bcb3
johed <johan.hedberg@episerver.com>
2017-01-25 13:51:10 +0100

    Quicksilver release version 10.2.2

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 
 
 
-[assembly: AssemblyVersion("10.2.1.0")]
-[assembly: AssemblyInformationalVersion("10.2.1")]
-[assembly: AssemblyFileVersion("10.2.1.2059")]
+[assembly: AssemblyVersion("10.2.2.0")]
+[assembly: AssemblyInformationalVersion("10.2.2")]
+[assembly: AssemblyFileVersion("10.2.2.2070")]
