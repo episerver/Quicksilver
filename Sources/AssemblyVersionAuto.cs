6105c4d81bf984e6f4d64d7bb36312d55fdd6e71
Per Gunsarfs <per.gunsarfs@episerver.com>
2017-08-21 12:52:25 +0200

    Quicksilver release version 11.1.1

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 
 
 
-[assembly: AssemblyVersion("11.1.0.0")]
-[assembly: AssemblyInformationalVersion("11.1.0")]
-[assembly: AssemblyFileVersion("11.1.0.2597")]
+[assembly: AssemblyVersion("11.1.1.0")]
+[assembly: AssemblyInformationalVersion("11.1.1")]
+[assembly: AssemblyFileVersion("11.1.1.2602")]
