554558827d7205e5b0fc10b1f3a01b7264a54f54
Per Gunsarfs <per.gunsarfs@episerver.com>
2018-01-30 09:33:30 +0100

    Quicksilver release version 11.7.1

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2018 Episerver")]
-[assembly: AssemblyVersion("11.7.0.0")]
-[assembly: AssemblyInformationalVersion("11.7.0")]
-[assembly: AssemblyFileVersion("11.7.0.2869")]
+[assembly: AssemblyVersion("11.7.1.0")]
+[assembly: AssemblyInformationalVersion("11.7.1")]
+[assembly: AssemblyFileVersion("11.7.1.2891")]
--- Sources/EPiServer.Reference.Commerce.Manager/Apps/Catalog/Scripts/CatalogClient.js
+++ Sources/EPiServer.Reference.Commerce.Manager/Apps/Catalog/Scripts/CatalogClient.js
@@ -1,57 +1,49 @@
 ﻿// JScript File
-function Mediachase_CatalogClient()
-{
+function Mediachase_CatalogClient() {
     // Properties
-    
+
     // Method Mappings   
-    this.CreateCatalog = function(source)
-    {
+    this.CreateCatalog = function (source) {
         CSManagementClient.ChangeView('Catalog', 'Edit');
-    }; 
-        
-    this.CreateNode = function(source)
-    {
-        var catalogid = CSManagementClient.QueryString("catalogid");
-        var catalognodeid = CSManagementClient.QueryString("catalognodeid");
-        CSManagementClient.ChangeView('Catalog', 'Node-Edit', 'catalogid='+catalogid+'&catalognodeid='+catalognodeid);
     };
 
-    this.CreateProductItem = function(source)
-    {
+    this.CreateNode = function (source) {
         var catalogid = CSManagementClient.QueryString("catalogid");
         var catalognodeid = CSManagementClient.QueryString("catalognodeid");
-        CSManagementClient.ChangeView('Catalog', 'Product-Edit', 'catalogid='+catalogid+'&catalognodeid='+catalognodeid+'&type=Product');
-    };    
-    
-    this.CreateVariationItem = function(source)
-    {
-        var catalogid = CSManagementClient.QueryString("catalogid");
-        var catalognodeid = CSManagementClient.QueryString("catalognodeid");
-        CSManagementClient.ChangeView('Catalog', 'Variation-Edit', 'catalogid='+catalogid+'&catalognodeid='+catalognodeid+'&type=Variation');
-    };        
+        CSManagementClient.ChangeView('Catalog', 'Node-Edit', 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid);
+    };
 
-    this.CreatePackageItem = function(source)
-    {
+    this.CreateProductItem = function (source) {
+        var catalogid = CSManagementClient.QueryString("catalogid");
+        var catalognodeid = CSManagementClient.QueryString("catalognodeid");
+        CSManagementClient.ChangeView('Catalog', 'Product-Edit', 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&type=Product');
+    };
+
+    this.CreateVariationItem = function (source) {
+        var catalogid = CSManagementClient.QueryString("catalogid");
+        var catalognodeid = CSManagementClient.QueryString("catalognodeid");
+        CSManagementClient.ChangeView('Catalog', 'Variation-Edit', 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&type=Variation');
+    };
+
+    this.CreatePackageItem = function (source) {
         var catalogid = CSManagementClient.QueryString("catalogid");
         var catalognodeid = CSManagementClient.QueryString("catalognodeid");
         CSManagementClient.ChangeView('Catalog', 'CatalogPackage-Edit', 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&type=Package');
-    };        
-
-    this.CreateProductBundleItem = function(source)
-    {
-        var catalogid = CSManagementClient.QueryString("catalogid");
-        var catalognodeid = CSManagementClient.QueryString("catalognodeid");
-        CSManagementClient.ChangeView('Catalog', 'Bundle-Edit', 'catalogid='+catalogid+'&catalognodeid='+catalognodeid+'&type=Bundle');
-    };        
-    
-    this.CreateDynPackageItem = function(source)
-    {
-        var catalogid = CSManagementClient.QueryString("catalogid");
-        var catalognodeid = CSManagementClient.QueryString("catalognodeid");
-        CSManagementClient.ChangeView('Catalog', 'DynamicPackage-Edit', 'catalogid='+catalogid+'&catalognodeid='+catalognodeid+'&type=DynamicPackage');
     };
 
-    this.OpenItem = function(type, id, extParams) {
+    this.CreateProductBundleItem = function (source) {
+        var catalogid = CSManagementClient.QueryString("catalogid");
+        var catalognodeid = CSManagementClient.QueryString("catalognodeid");
+        CSManagementClient.ChangeView('Catalog', 'Bundle-Edit', 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&type=Bundle');
+    };
+
+    this.CreateDynPackageItem = function (source) {
+        var catalogid = CSManagementClient.QueryString("catalogid");
+        var catalognodeid = CSManagementClient.QueryString("catalognodeid");
+        CSManagementClient.ChangeView('Catalog', 'DynamicPackage-Edit', 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&type=DynamicPackage');
+    };
+
+    this.OpenItem = function (type, id, extParams) {
         type = type.trim().toLowerCase();
         var catalogid = CSManagementClient.QueryString("catalogid");
         var catalognodeid = CSManagementClient.QueryString("catalognodeid");
@@ -61,7 +53,7 @@ function Mediachase_CatalogClient()
         else if (type == 'variation')
             CSManagementClient.ChangeView("Catalog", "Variation-Edit", 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&catalogentryid=' + id + '&' + extParams);
         else if (type == 'package')
-        	CSManagementClient.ChangeView("Catalog", "CatalogPackage-Edit", 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&catalogentryid=' + id + '&' + extParams);
+            CSManagementClient.ChangeView("Catalog", "CatalogPackage-Edit", 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&catalogentryid=' + id + '&' + extParams);
         else if (type == 'bundle')
             CSManagementClient.ChangeView("Catalog", "Bundle-Edit", 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&catalogentryid=' + id + '&' + extParams);
         else if (type == 'dynamicpackage')
@@ -73,107 +65,87 @@ function Mediachase_CatalogClient()
                 CSManagementClient.ChangeView("Catalog", "Node-Edit", 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid + '&nodeid=' + id);
         }
     };
-    
-    this.ViewItem = function(type, id)
-    {
+
+    this.ViewItem = function (type, id) {
         type = type.trim().toLowerCase();
         var catalogid = CSManagementClient.QueryString("catalogid");
-       
-        if(type=='node')
-            CSManagementClient.ChangeView("Catalog","Node-List",'catalogid='+catalogid+'&catalognodeid='+id);
+
+        if (type == 'node')
+            CSManagementClient.ChangeView("Catalog", "Node-List", 'catalogid=' + catalogid + '&catalognodeid=' + id);
         else
             this.OpenItem(type, id);
     };
-    
-    this.OpenItem2 = function(params)
-    {
+
+    this.OpenItem2 = function (params) {
         var id = '';
         var type = '';
-        try
-        {
+        try {
             var cmdObj = Sys.Serialization.JavaScriptSerializer.deserialize(params);
             id = cmdObj.CommandArguments.ID;
             type = cmdObj.CommandArguments.Type;
         }
-        catch(e)
-        {
+        catch (e) {
             alert('A problem occured with retrieving parameters for function OpenItem');
             return;
         }
         this.OpenItem(type, id);
     };
-    
-    this.EditCatalog = function(params)
-    {
+
+    this.EditCatalog = function (params) {
         var id = '';
-        
-        try
-        {
+
+        try {
             var cmdObj = Sys.Serialization.JavaScriptSerializer.deserialize(params);
             id = cmdObj.CommandArguments.CatalogId;
         }
-        catch(e)
-        {
+        catch (e) {
             alert('A problem occured with retrieving parameters for function EditCatalog');
             return;
         }
-        
-        CSManagementClient.ChangeView("Catalog","Edit",'catalogid='+id);
-    };        
-    
-    this.MoveCopyOpenDialog = function(source)
-    {
-        if(CSManagementClient.ListHasItemsSelected(source))
-        {            
+
+        CSManagementClient.ChangeView("Catalog", "Edit", 'catalogid=' + id);
+    };
+
+    this.MoveCopyOpenDialog = function (source) {
+        if (CSManagementClient.ListHasItemsSelected(source)) {
             MoveCopy_OpenDialog();
         }
     };
-    
-    this.CSVImportCatalog = function()
-    {
+
+    this.CSVImportCatalog = function () {
         CSManagementClient.ChangeView('Catalog', 'Catalog-CSVImport', '');
     };
-    
-    this.ImportCatalog = function()
-    {
+
+    this.ImportCatalog = function () {
         CSManagementClient.ChangeView('Catalog', 'Catalog-Import', '');
     };
-    
-    this.ExportCatalog = function(params)
-    {
-        if (params != null)
-        {
+
+    this.ExportCatalog = function (params) {
+        if (params != null) {
             var items = Toolbar_GetSelectedGridItems(params);
-            
-            if (items)
-            {
+            if (items) {
                 var splitter = ';';
-                
+
                 // remove trailing splitter
-                if( (items.length > 1) && (items.lastIndexOf(splitter) == items.length-1) )
-                    items = items.substring(0, items.length-1);
+                if ((items.length > 1) && (items.lastIndexOf(splitter) == items.length - 1))
+                    items = items.substring(0, items.length - 1);
 
                 // get array of selected items
                 var selectedItems = items.split(splitter);
-                if(selectedItems.length == 0)
-                {
+                if (selectedItems.length == 0) {
                     alert("You must select a catalog before you can perform export.");
                     return;
                 }
-                else if(selectedItems.length > 1)
-                {
+                else if (selectedItems.length > 1) {
                     alert('You must select only one catalog!');
                     return;
                 }
-                else if(selectedItems.length == 1)
-                {
+                else if (selectedItems.length == 1) {
                     var keys = selectedItems[0].split(CSManagementClient.EcfListView_PrimaryKeySeparator);
-                    if(keys && (keys.length >= 2))
-                    {
+                    if (keys && (keys.length >= 1)) {
                         var id = keys[0];
-                        var name = keys[1];
-                    
-                        CSManagementClient.ChangeView('Catalog', 'Catalog-Export', 'catalogid='+id+'&catalogname='+encodeURI(name));
+
+                        CSManagementClient.ChangeView('Catalog', 'Catalog-Export', 'catalogid=' + id);
                     }
                     else
                         alert('Invalid primaryKey!');
@@ -182,60 +154,49 @@ function Mediachase_CatalogClient()
             }
         }
     };
-    
-    this.ExportCatalog2 = function(params)
-    {
+
+    this.ExportCatalog2 = function (params) {
         var catalogId = '';
-        var catalogName = '';
-        try
-        {
+        try {
             var cmdObj = Sys.Serialization.JavaScriptSerializer.deserialize(params);
-            catalogId = cmdObj.CommandArguments.SiteId;
-            catalogName = cmdObj.CommandArguments.Name;
+            catalogId = cmdObj.CommandArguments.CatalogId;
         }
-        catch(e)
-        {
+        catch (e) {
             alert('A problem occured with retrieving parameters for function ExportCatalog2');
             return;
         }
-        CSManagementClient.ChangeView('Catalog', 'Catalog-Export', 'catalogid='+catalogId+'&catalogname='+encodeURI(catalogName));
+        CSManagementClient.ChangeView('Catalog', 'Catalog-Export', 'catalogid=' + catalogId);
     };
 
-    this.CatalogSaveRedirect = function(catalogid, catalognodeid) {
+    this.CatalogSaveRedirect = function (catalogid, catalognodeid) {
         var catalogid = catalogid != null && catalogid.length > 0 ? catalogid : CSManagementClient.QueryString("catalogid");
         var catalognodeid = catalognodeid != null && catalognodeid.length > 0 ? catalognodeid : CSManagementClient.QueryString("catalognodeid");
         CSManagementClient.ChangeView('Catalog', 'Node-List', 'catalogid=' + catalogid + '&catalognodeid=' + catalognodeid);
     };
-    
+
     //-------------------------- Currencies -------------------------------------
-    this.NewCurrency = function()
-    {
+    this.NewCurrency = function () {
         CSManagementClient.ChangeView('Catalog', 'Currency-Edit', '');
     };
-    
-    this.EditCurrency = function(currencyId)
-    {
-        CSManagementClient.ChangeView('Catalog', 'Currency-Edit', 'currencyid='+currencyId);
+
+    this.EditCurrency = function (currencyId) {
+        CSManagementClient.ChangeView('Catalog', 'Currency-Edit', 'currencyid=' + currencyId);
     };
-    
-    this.EditCurrency2 = function(params)
-    {
+
+    this.EditCurrency2 = function (params) {
         var currencyId = '';
-        try
-        {
+        try {
             var cmdObj = Sys.Serialization.JavaScriptSerializer.deserialize(params);
             currencyId = cmdObj.CommandArguments.CurrencyId;
         }
-        catch(e)
-        {
+        catch (e) {
             alert('A problem occured with retrieving parameters for method EditCurrency2');
             return;
         }
         this.EditCurrency(currencyId);
     };
-    
-    this.CurrencySaveRedirect = function()
-    {
+
+    this.CurrencySaveRedirect = function () {
         CSManagementClient.ChangeView('Catalog', 'Currencies-List', '');
     };
 };
