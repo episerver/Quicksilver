6dbd3923b070e7ae6da59b32f77a01a25d87545d
bing <bien.nguyen@episerver.com>
2018-02-06 13:36:31 +0700

    Quicksilver release version 11.8.0

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2018 Episerver")]
-[assembly: AssemblyVersion("11.7.1.0")]
-[assembly: AssemblyInformationalVersion("11.7.1")]
-[assembly: AssemblyFileVersion("11.7.1.2891")]
+[assembly: AssemblyVersion("11.8.0.0")]
+[assembly: AssemblyInformationalVersion("11.8.0")]
+[assembly: AssemblyFileVersion("11.8.0.2905")]
