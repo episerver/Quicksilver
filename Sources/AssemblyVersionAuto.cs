06ca79a5944879c0c2be4ba23a08157e2cf04a8c
Magnus Rahl <magnus.rahl@episerver.com>
2018-08-21 08:18:11 +0200

    Quicksilver release 12.6.0

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2018 Episerver")]
-[assembly: AssemblyVersion("12.5.0.0")]
-[assembly: AssemblyInformationalVersion("12.5.0")]
-[assembly: AssemblyFileVersion("12.5.0.3288")]
+[assembly: AssemblyVersion("12.6.0.0")]
+[assembly: AssemblyInformationalVersion("12.6.0")]
+[assembly: AssemblyFileVersion("12.6.0.3305")]
