a2c6862bbeb922544a0b1fafd0c5eedf30624236
Giang Tran <giang.tran@episerver.com>
2018-08-27 17:33:07 +0700

    Quicksilver release version 12.7.0

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2018 Episerver")]
-[assembly: AssemblyVersion("12.6.0.0")]
-[assembly: AssemblyInformationalVersion("12.6.0")]
-[assembly: AssemblyFileVersion("12.6.0.3305")]
+[assembly: AssemblyVersion("12.7.0.0")]
+[assembly: AssemblyInformationalVersion("12.7.0")]
+[assembly: AssemblyFileVersion("12.7.0.3308")]
