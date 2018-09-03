6310524bfdb15264a32e29ed09221b4b41c266a5
Gonzalo Gonzalez <gonzalo.gonzalez@episerver.com>
2018-09-03 11:03:34 +0200

    QuickSilver release version 12.8.0

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2018 Episerver")]
-[assembly: AssemblyVersion("12.7.0.0")]
-[assembly: AssemblyInformationalVersion("12.7.0")]
-[assembly: AssemblyFileVersion("12.7.0.3308")]
+[assembly: AssemblyVersion("12.8.0.0")]
+[assembly: AssemblyInformationalVersion("12.8.0")]
+[assembly: AssemblyFileVersion("12.8.0.3314")]
