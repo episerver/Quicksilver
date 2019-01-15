11031c6193a6fe662e514a97d19b4a8e89311514
Gonzalo Gonzalez <gonzalo.gonzalez@episerver.com>
2019-01-15 11:00:55 +0100

    Quicksilver release version 12.16.0

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2019 Episerver")]
-[assembly: AssemblyVersion("12.15.1.0")]
-[assembly: AssemblyInformationalVersion("12.15.1")]
-[assembly: AssemblyFileVersion("12.15.1.3487")]
+[assembly: AssemblyVersion("12.16.0.0")]
+[assembly: AssemblyInformationalVersion("12.16.0")]
+[assembly: AssemblyFileVersion("12.16.0.3495")]
