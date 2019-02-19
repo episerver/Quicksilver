8be98521f60016ebe4e2dfabdff639959f7f6083
Cuong Vu <Cuong.Vu@episerver.com>
2019-02-19 11:27:08 +0700

    Quicksilver release version 12.17.0

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2019 Episerver")]
-[assembly: AssemblyVersion("12.16.0.0")]
-[assembly: AssemblyInformationalVersion("12.16.0")]
-[assembly: AssemblyFileVersion("12.16.0.3495")]
+[assembly: AssemblyVersion("12.17.0.0")]
+[assembly: AssemblyInformationalVersion("12.17.0")]
+[assembly: AssemblyFileVersion("12.17.0.3530")]
