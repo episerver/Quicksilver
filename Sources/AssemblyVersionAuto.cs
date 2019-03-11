3f5ed2144e3f9c8db12150f25ea59b2a9289522c
eriska <erik.skagerlind@episerver.com>
2019-03-11 16:00:58 +0100

    Quicksilver release version 12.17.2

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2019 Episerver")]
-[assembly: AssemblyVersion("12.17.1.0")]
-[assembly: AssemblyInformationalVersion("12.17.1")]
-[assembly: AssemblyFileVersion("12.17.1.3556")]
+[assembly: AssemblyVersion("12.17.2.0")]
+[assembly: AssemblyInformationalVersion("12.17.2")]
+[assembly: AssemblyFileVersion("12.17.2.3575")]
