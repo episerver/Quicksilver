8c7f7be25380ddf617dd026b45aa94fdab1570b2
Vinh Luong <vinh.luong@episerver.com>
2019-06-11 10:54:43 +0700

    Quicksilver release version 13.3.1

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2019 Episerver")]
-[assembly: AssemblyVersion("13.3.0.0")]
-[assembly: AssemblyInformationalVersion("13.3.0")]
-[assembly: AssemblyFileVersion("13.3.0.3661")]
+[assembly: AssemblyVersion("13.3.1.0")]
+[assembly: AssemblyInformationalVersion("13.3.1")]
+[assembly: AssemblyFileVersion("13.3.1.3669")]
