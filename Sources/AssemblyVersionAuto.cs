992180e32c2e3d214f0b61a287cdce340a7f39ed
Cuong Vu <Cuong.Vu@episerver.com>
2019-11-27 10:48:32 +0700

    Quicksilver release version 13.12.0

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright ? 2019 Episerver")]
-[assembly: AssemblyVersion("13.11.0.0")]
-[assembly: AssemblyInformationalVersion("13.11.0")]
-[assembly: AssemblyFileVersion("13.11.0.3856")]
+[assembly: AssemblyVersion("13.12.0.0")]
+[assembly: AssemblyInformationalVersion("13.12.0")]
+[assembly: AssemblyFileVersion("13.12.0.3864")]
