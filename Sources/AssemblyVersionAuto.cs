dbc2e588829db6fa292b31a9570adfe2d5b4d8e8
Viet Anh Nguyen <vietanh.nguyen@episerver.com>
2020-01-21 13:58:19 +0700

    Quicksilver release version 13.15.0

--- Sources/AssemblyVersionAuto.cs
+++ Sources/AssemblyVersionAuto.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 
 
-[assembly: AssemblyCopyright("Copyright ? 2019 Episerver")]
-[assembly: AssemblyVersion("13.14.0.0")]
-[assembly: AssemblyInformationalVersion("13.14.0")]
-[assembly: AssemblyFileVersion("13.14.0.3896")]
+[assembly: AssemblyCopyright("Copyright ? 2020 Episerver")]
+[assembly: AssemblyVersion("13.15.0.0")]
+[assembly: AssemblyInformationalVersion("13.15.0")]
+[assembly: AssemblyFileVersion("13.15.0.3942")]
